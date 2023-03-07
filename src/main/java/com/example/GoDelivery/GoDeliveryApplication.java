7f30b4e5289a2ceb171ec8c1893ed43e4b4d8bf5
mseyeong <mseyeong3131@gmail.com>
2023-03-07 21:26:45 +0900

    Move Repository

---
+++ src/main/java/com/example/GoDelivery/GoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoDelivery/GoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
